2654f55e844addc088b4df87b33a3de01feaf985
sherwin cheng <oycspjn111@126.com>
2016-09-10 01:12:55 +0800

    add videoangular module

--- js/features/home/controller/HomeController.js
+++ js/features/home/controller/HomeController.js
@@ -9,9 +9,26 @@
 class HomeController {
 
     /*@ngInject*/
-    constructor($scope) {
+    constructor($scope,$sce) {
         this.$scope = $scope;
-
+        this.aaa = 1;
+        this.config = {
+            preload: "none",
+            sources: [
+                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+                {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+            ],
+            theme: {
+                url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+            },
+            plugins: {
+                controls: {
+                    autoHide: true,
+                    autoHideTime: 5000
+                }
+            }
+        };
         this._init_();
         this._destroy_();
     }
