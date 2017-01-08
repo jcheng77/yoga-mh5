02cc34c9dc7ab5011087aaa8f208f7cb2bd06824
sherwin cheng <oycspjn111@126.com>
2017-01-08 20:07:49 +0800

    add tages

--- js/features/LiveWork/service/LiveWorkService.js
+++ js/features/LiveWork/service/LiveWorkService.js
@@ -15,18 +15,25 @@ class LiveWorkService {
         this.utils = utils;
     }
 
-    getData() {
+    getData(tag) {
         const url = api.API_LIVE_WORK;
-        return this.$http.get(url).then(data => data.data).then(data => {
-            const swiper = data.filter(data => data.type === 0);
+        return this.$http.get(`${url}?tag=${tag}`).then(data => data.data).then(data => {
             const wait = data.filter(data => data.state === 0);
             const live = data.filter(data => data.state === 1);
             const over = data.filter(data => data.state === 2);
             return {
-                swiper:swiper,
                 wait:wait,
                 live:live,
                 over:over
+            }
+        });
+    }
+    getBanner() {
+        const url = api.API_LIVE_WORK;
+        return this.$http.get(`${url}?type=0`).then(data => data.data).then(data => {
+            const swiper = data.filter(data => data.type === 0);
+            return {
+                swiper:swiper
             }
         });
     }
